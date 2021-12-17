2be887047996e5490edabd18da2864d413625bd8
JL <34074989+commandline-be@users.noreply.github.com>
2021-12-17 13:22:59 +0100

    Update make_ncsc.sh
    added csirt parsing

--- log4_ncsc/make_ncsc.sh
+++ log4_ncsc/make_ncsc.sh
@@ -14,7 +14,9 @@ mkdir -p tmp
 ./scripts/clean_ncsc_software.sh ./tmp/ncsc_software.csv
 ./scripts/clean_ncsc_scanning.sh ./tmp/ncsc_scanning.csv
 ./scripts/clean_ncsc_mitigations.sh ./tmp/ncsc_mitigations.csv
+./scripts/clean_ncsc_csirts.sh ./tmp/ncsc_csirts.csv
 #./scripts/clean_ncsc_hunting.sh ./tmp/ncsc_hunting.csv
 #sed -i 's/|/\t/g' ./tmp/*.csv
 ssconvert -I Gnumeric_stf:stf_csvtab --merge-to=log4j_cve_2021_44228_$date.xlsx ./tmp/ncsc_ioc.csv ./tmp/ncsc_software.csv ./tmp/ncsc_scanning.csv ./tmp/ncsc_mitigations.csv
 rm -Rf ./tmp/*
+xdg-open ./log4j_cve_2021_44228_"$date".xlsx
