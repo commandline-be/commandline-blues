597dd89c0df4099360d850e7b722f2502b32c971
JL <34074989+commandline-be@users.noreply.github.com>
2021-12-17 13:24:45 +0100

    Update get_ncsc.sh
    added csirt, made script silent

--- log4_ncsc/scripts/get_ncsc.sh
+++ log4_ncsc/scripts/get_ncsc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/NCSC-NL/log4shell/main/iocs/README.md -O ./tmp/ncsc_ioc.csv
-wget https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/README.md -O ./tmp/ncsc_software.csv
-wget https://raw.githubusercontent.com/NCSC-NL/log4shell/main/scanning/README.md -O ./tmp/ncsc_scanning.csv
-wget https://github.com/NCSC-NL/log4shell/raw/main/mitigation/README.md -O ./tmp/ncsc_mitigations.csv
-wget https://github.com/NCSC-NL/log4shell/raw/main/hunting/README.md -O ./tmp/ncsc_hunting.csv
+wget -q https://raw.githubusercontent.com/NCSC-NL/log4shell/main/iocs/README.md -O ./tmp/ncsc_ioc.csv
+wget -q https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/README.md -O ./tmp/ncsc_software.csv
+wget -q https://raw.githubusercontent.com/NCSC-NL/log4shell/main/scanning/README.md -O ./tmp/ncsc_scanning.csv
+wget -q https://github.com/NCSC-NL/log4shell/raw/main/mitigation/README.md -O ./tmp/ncsc_mitigations.csv
+wget -q https://github.com/NCSC-NL/log4shell/raw/main/hunting/README.md -O ./tmp/ncsc_hunting.csv
+wget -q https://raw.githubusercontent.com/enisaeu/CNW/main/log4shell/README.md -O ./tmp/ncsc_csirts.csv
