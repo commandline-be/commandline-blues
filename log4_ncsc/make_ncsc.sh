888b7c4d3d35b2af019e06652d99dceffcdf82c2
JL <34074989+commandline-be@users.noreply.github.com>
2021-12-17 12:55:51 +0100

    Create make_ncsc.sh
    main script to gather data, parse data, collect data, create spreadsheet using ssconvert

---
+++ log4_ncsc/make_ncsc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##  make ncsc spreadsheet
+
+today=`date +%d`
+month=`date +%m`
+years=`date +%y`
+date="$years"_"$month"_"$today"
+echo "$date"
+
+./scripts/get_ncsc.sh
+./scripts/clean_ncsc_ioc.sh ./tmp/ncsc_ioc.csv
+./scripts/clean_ncsc_software.sh ./tmp/ncsc_software.csv
+./scripts/clean_ncsc_scanning.sh ./tmp/ncsc_scanning.csv
+./scripts/clean_ncsc_mitigations.sh ./tmp/ncsc_mitigations.csv
+#./scripts/clean_ncsc_hunting.sh ./tmp/ncsc_hunting.csv
+#sed -i 's/|/\t/g' ./tmp/*.csv
+ssconvert -I Gnumeric_stf:stf_csvtab --merge-to=log4j_cve_2021_44228_$date.xlsx ./tmp/ncsc_ioc.csv ./tmp/ncsc_software.csv ./tmp/ncsc_scanning.csv ./tmp/ncsc_mitigations.csv
+rm -Rf ./tmp/*
